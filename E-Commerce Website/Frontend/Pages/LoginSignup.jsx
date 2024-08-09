db917413959ddb65c874c00c907f4feeb1624f28
Venkatesh Divvala <159468906+VenkateshDivvala@users.noreply.github.com>
2024-08-09 20:01:34 +0530

    Add files via upload

---
+++ E-Commerce Website/Frontend/Pages/LoginSignup.jsx
@@ -0,0 +1,22 @@
+import './Css/LoginSignup.css'
+const LoginSignup = () => {
+    return (
+        <div className="loginsignup">
+          <div className="loginsignup-container">
+            <h1>Sign Up</h1>
+            <div className="loginsignup-fields">
+                <input type="text" placeholder="Enter Your name"/>
+                <input type="email" placeholder="Enter Your mail id"/>
+                <input type="password" placeholder="Password"/>
+            </div>
+            <button>Continue</button>
+            <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+            <div className="loginsignup-agree">
+                <input type="checkbox" name="" id=""/>
+                <p>By continuing, I agree to the terms and conditions</p>
+            </div>
+          </div>
+        </div>
+    )
+}
+export default LoginSignup
---
+++ E-Commerce Website/Frontend/Pages/Product.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import { ShopContext } from "../Context/ShopContext"
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts"
+const Product = () => {
+    const {allProduct}=useContext(ShopContext);
+    const {productId} = useParams();
+    const product=allProduct.find((e)=>e.id === Number(productId))
+    return (
+        <div>
+            <Breadcrum product={product}/>
+            <ProductDisplay product={product}/>
+            <DescriptionBox/>
+            <RelatedProducts/>
+        </div>
+    )
+}
+export default Product
