a5c2e9e929df96520e5575b4c45c2e533fca4a99
Venkatesh Divvala <159468906+VenkateshDivvala@users.noreply.github.com>
2024-08-09 19:47:17 +0530

    Add files via upload

---
+++ E-Commerce Website/Frontend/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import './DescriptionBox.css'
+const DescriptionBox=()=>{
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>
+                    bshcbjc scbk jsnsfwdwn jnnncjncwn jnwncn jcncnwncjnc skjscnjncwn onocwen cjcnw
+                    sdc wjcwcniu jcnjcnwjcnnvsdnfnj ssnsn msdjndlckl nocnnc njnj jsncj sdkjcnj
+                    sdc cscbsdkcsdcscscnsdl snsnsdklvmsvnslvkl svklv svs svs kmlsv skdvkmsklnsfdsklwi
+                    ss jksnvjsnnvfefvejdjwncn
+                </p>
+                <p>
+                bshcbjc scbk jsnsfwdwn jnnncjncwn jnwncn jcncnwncjnc skjscnjncwn onocwen cjcnw
+                sdc wjcwcniu jcnjcnwjcnnvsdnfnj ssnsn msdjndlckl
+                </p>
+            </div>
+        </div>
+    )
+}
+export default DescriptionBox
